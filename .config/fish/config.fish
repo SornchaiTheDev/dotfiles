57d3e3678f4d8097a43c6b7e5eeb5a8554f66651
SornchaiTheDev <sornchaithedev@gmail.com>
2024-12-29 17:06:22 +0700

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ starship init fish | source
 zoxide init --cmd cd fish | source
 
 # Paths
-set -x STARSHIP_CONFIG ../starship/starship.toml
+set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
 
 # pnpm
 set -gx PNPM_HOME "/Users/sornchaisomsakul/Library/pnpm"
@@ -10,3 +10,20 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+function ls
+  command eza --icons $argv
+end
+
+function ta
+  command tmux a
+end
+
+# Load pyenv automatically by adding
+# the following to ~/.config/fish/config.fish:
+
+status --is-interactive; and . (pyenv init -|psub)
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
